39e1a839d84b3d3169c6db944d72d9eae675837f
happylich <wangtianyu00@163.com>
2018-11-07 22:56:39 +0800

    修改图片适配性

--- js/action.js
+++ js/action.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
     img = "bg0" + parseInt(Math.random()*5)
     background_image = "url(img/" + img + ".jpg)"
     $(".bg").css("background-image", background_image);
-    $(".bg").addClass(img)
+    $(".content").addClass(img)
 });
