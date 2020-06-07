c42ab93068a28a92d6e0a47a888a280a1000f238
happylich <wangtianyu00@163.com>
2020-06-07 14:02:38 +0800

    增加刷新按钮

---
+++ action.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+})
+
+$("div.refresh").click(function() {
+    var oldlink = document.getElementsByTagName("link").item(1);
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    var styleList = ['style.css', 'style1.css']
+    newlink.setAttribute("href", styleList[Math.floor((Math.random()*styleList.length))]);
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+})
